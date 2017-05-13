fe37ec035040c090db3610ff8d4e696ef0293180
endymecy <endymecy@sina.cn>
2017-05-13 13:44:09 +0800

    add MLPMnistTwoLayerExample

--- dl4scala-examples/src/main/scala/org/dl4scala/examples/feedforward/mnist/MLPMnistTwoLayerExample.scala
+++ dl4scala-examples/src/main/scala/org/dl4scala/examples/feedforward/mnist/MLPMnistTwoLayerExample.scala
@@ -1,25 +1,22 @@
 package org.dl4scala.examples.feedforward.mnist
 
-import org.slf4j.LoggerFactory
-import org.slf4j.Logger
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator
 import org.deeplearning4j.eval.Evaluation
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
-import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.deeplearning4j.nn.api.OptimizationAlgorithm
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration
-import org.deeplearning4j.nn.conf.Updater
-import org.deeplearning4j.nn.conf.layers.DenseLayer
-import org.deeplearning4j.nn.conf.layers.OutputLayer
+import org.deeplearning4j.nn.conf.layers.{DenseLayer, OutputLayer}
+import org.deeplearning4j.nn.conf.{NeuralNetConfiguration, Updater}
 import org.deeplearning4j.nn.weights.WeightInit
 import org.nd4j.linalg.activations.Activation
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
+import org.slf4j.{Logger, LoggerFactory}
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 
 /**
   * Created by endy on 2017/5/13.
   */
-object MLPMnistSingleLayerExample extends App{
-  private val log: Logger = LoggerFactory.getLogger(MLPMnistSingleLayerExample.getClass)
+object MLPMnistTwoLayerExample extends App{
+  private val log: Logger = LoggerFactory.getLogger(MLPMnistTwoLayerExample.getClass)
 
   // number of rows and columns in the input pictures//number of rows and columns in the input pictures
   private  val numRows = 28
@@ -28,11 +25,11 @@ object MLPMnistSingleLayerExample extends App{
   private  val batchSize = 128 // batch size for each epoch
   private  val rngSeed = 123 // random number seed for reproducibility
   private  val numEpochs = 15 // number of epochs to perform
+  private  val rate: Double = 0.0015 // learning rate
 
   // Get the DataSetIterators://Get the DataSetIterators:
   private val mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed)
   private val mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed)
-
 
   log.info("Build model....")
 
@@ -41,29 +38,32 @@ object MLPMnistSingleLayerExample extends App{
     .seed(rngSeed) // include a random seed for reproducibility
     .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT) // use stochastic gradient descent as an optimization algorithm
     .iterations(1)
-    .learningRate(0.006) // specify the learning rate
-    .updater(Updater.NESTEROVS).momentum(0.9) // specify the rate of change of the learning rate.
-    .regularization(true).l2(1e-4)
+    .activation(Activation.RELU)
+    .weightInit(WeightInit.XAVIER)
+    .learningRate(rate) // specify the learning rate
+    .updater(Updater.NESTEROVS).momentum(0.98) // specify the rate of change of the learning rate.
+    .regularization(true).l2(rate * 0.005) // regularize learning model
     .list()
-    .layer(0, new DenseLayer.Builder() // create the first, input layer with xavier initialization
-        .nIn(numRows * numColumns)
-        .nOut(1000)
-        .activation(Activation.RELU)
-        .weightInit(WeightInit.XAVIER)
-        .build())
-    .layer(1, new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD) // create hidden layer
-        .nIn(1000)
-        .nOut(outputNum)
-        .activation(Activation.SOFTMAX)
-        .weightInit(WeightInit.XAVIER)
-        .build())
-    .pretrain(false).backprop(true) // use backpropagation to adjust weights
+    .layer(0, new DenseLayer.Builder() // create the first input layer.
+      .nIn(numRows * numColumns)
+      .nOut(500)
+      .build())
+    .layer(1, new DenseLayer.Builder() // create the second input layer
+      .nIn(500)
+      .nOut(100)
+      .build())
+    .layer(2, new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD) // create hidden layer
+      .activation(Activation.SOFTMAX)
+      .nIn(100)
+      .nOut(outputNum)
+      .build())
+    .pretrain(false).backprop(true)
     .build()
 
-  private val model = new MultiLayerNetwork(conf)
+  val model = new MultiLayerNetwork(conf)
   model.init()
-  // print the score with every 1 iteration
-  model.setListeners(new ScoreIterationListener(1))
+  model.setListeners(new ScoreIterationListener(5)) // print the score with every iteration
+
   log.info("Train model....")
 
   for(i <- 0 until numEpochs){
@@ -81,5 +81,4 @@ object MLPMnistSingleLayerExample extends App{
 
   log.info(eval.stats)
   log.info("****************Example finished********************")
-
 }
