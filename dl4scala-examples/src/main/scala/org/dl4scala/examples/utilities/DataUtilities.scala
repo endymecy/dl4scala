8b759def4d5cb21bff336460d02c2b4dd120b1d7
endymecy <endymecy@sina.cn>
2017-05-26 14:48:50 +0800

    add DataUtilities

---
+++ dl4scala-examples/src/main/scala/org/dl4scala/examples/utilities/DataUtilities.scala
@@ -0,0 +1,49 @@
+package org.dl4scala.examples.utilities
+
+import java.io._
+
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import org.slf4j.{Logger, LoggerFactory}
+
+/**
+  * Created by endy on 2017/5/26.
+  */
+object DataUtilities {
+  val logger: Logger = LoggerFactory.getLogger(DataUtilities.getClass)
+  private val BUFFER_SIZE = 4096
+
+  @throws(classOf[IOException])
+  def extractTarGz(filePath: String, outputPath: String): Unit = {
+    val fileCount = 0
+    var dirCount = 0
+
+    logger.info("Extracting files")
+
+    val tais = new TarArchiveInputStream(new GzipCompressorInputStream(
+      new BufferedInputStream(new FileInputStream(filePath))))
+    // Read the tar entries using the getNextEntry method
+    var entry = tais.getNextTarEntry
+    while (entry != null) {
+      //Create directories as required//Create directories as required
+      if (entry.isDirectory) {
+        new File(outputPath + entry.getName).mkdirs
+        dirCount += 1
+      } else {
+        var count = 0
+        val data = new Array[Byte](BUFFER_SIZE)
+        val fos = new FileOutputStream(outputPath + entry.getName)
+        val dest = new BufferedOutputStream(fos, BUFFER_SIZE)
+        while ((count = tais.read(data, 0, BUFFER_SIZE)) == -1) {
+          dest.write(data, 0, count)
+        }
+        dest.close()
+
+      }
+      entry = tais.getNextTarEntry
+      if (fileCount % 1000 == 0) logger.info(".")
+    }
+
+    tais.close()
+  }
+}
