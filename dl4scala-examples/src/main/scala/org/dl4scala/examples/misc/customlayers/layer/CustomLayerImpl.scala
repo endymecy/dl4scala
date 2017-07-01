59d7addb453866a599b6a179cd6770f791faf599
endymecy <endymecy@sina.cn>
2017-07-01 11:02:56 +0800

    add CustomLayerImpl

---
+++ dl4scala-examples/src/main/scala/org/dl4scala/examples/misc/customlayers/layer/CustomLayerImpl.scala
@@ -0,0 +1,72 @@
+package org.dl4scala.examples.misc.customlayers.layer
+
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration
+import org.deeplearning4j.nn.gradient.{DefaultGradient, Gradient}
+import org.deeplearning4j.nn.layers.BaseLayer
+import org.deeplearning4j.nn.params.DefaultParamInitializer
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.factory.Nd4j
+import org.nd4j.linalg.indexing.NDArrayIndex
+
+/**
+  * Created by endy on 2017/7/1.
+  */
+class CustomLayerImpl(conf: NeuralNetConfiguration) extends BaseLayer[CustomLayer](conf) {
+  override def isPretrainLayer: Boolean = false
+
+  override def preOutput(x: INDArray, training: Boolean): INDArray = {
+    super.preOutput(x, training)
+  }
+
+  override def activate(training: Boolean): INDArray = {
+    val output = preOutput(training)
+    val columns = output.columns
+
+    val firstHalf = output.get(NDArrayIndex.all, NDArrayIndex.interval(0, columns / 2))
+    val secondHalf = output.get(NDArrayIndex.all, NDArrayIndex.interval(columns / 2, columns))
+
+    val activation1 = conf.getLayer.getActivationFn
+    val activation2 = conf.getLayer.asInstanceOf[CustomLayer].getSecondActivationFunction
+
+    // IActivation function instances modify the activation functions in-place
+    activation1.getActivation(firstHalf, training)
+    activation2.getActivation(secondHalf, training)
+    output
+  }
+
+  override def backpropGradient(epsilon: INDArray): (Gradient, INDArray) = {
+    val activationDerivative = preOutput(true)
+    val columns = activationDerivative.columns
+
+    val firstHalf = activationDerivative.get(NDArrayIndex.all, NDArrayIndex.interval(0, columns / 2))
+    val secondHalf = activationDerivative.get(NDArrayIndex.all, NDArrayIndex.interval(columns / 2, columns))
+
+    val epsilonFirstHalf = epsilon.get(NDArrayIndex.all, NDArrayIndex.interval(0, columns / 2))
+    val epsilonSecondHalf = epsilon.get(NDArrayIndex.all, NDArrayIndex.interval(columns / 2, columns))
+
+    val activation1 = conf.getLayer.getActivationFn
+    val activation2 = conf.getLayer.asInstanceOf[CustomLayer].getSecondActivationFunction
+
+    //IActivation backprop method modifies the 'firstHalf' and 'secondHalf' arrays in-place, to contain dL/dz
+    activation1.backprop(firstHalf, epsilonFirstHalf)
+    activation2.backprop(secondHalf, epsilonSecondHalf)
+
+
+    if (maskArray != null) activationDerivative.muliColumnVector(maskArray)
+
+    val ret = new DefaultGradient
+
+    val weightGrad = gradientViews.get(DefaultParamInitializer.WEIGHT_KEY) //f order
+    Nd4j.gemm(input, activationDerivative, weightGrad, true, false, 1.0, 0.0)
+    val biasGrad = gradientViews.get(DefaultParamInitializer.BIAS_KEY)
+    biasGrad.assign(activationDerivative.sum(0)) //TODO: do this without the assign
+
+
+    ret.gradientForVariable.put(DefaultParamInitializer.WEIGHT_KEY, weightGrad)
+    ret.gradientForVariable.put(DefaultParamInitializer.BIAS_KEY, biasGrad)
+
+    val epsilonNext = params.get(DefaultParamInitializer.WEIGHT_KEY).mmul(activationDerivative.transpose).transpose
+
+    (ret, epsilonNext)
+  }
+}
